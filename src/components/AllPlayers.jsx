566981964d55c86b411103a816cb6dabdacd097e
Stacey Sanchez <staceysanchez@macbook-air.mynetworksettings.com>
2023-10-04 01:51:28 -0400

    conditional added in case state is null

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,9 +1,9 @@
-import {react, useEffect, useState} from "react";
-import {useNavigate} from 'react-router-dom';
+import { react, useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import SinglePlayer from "./SinglePlayer";
 
 
-export default function AllPlayers () {
+export default function AllPlayers() {
     const [playerList, setPlayerList] = useState(null);
     const navigate = useNavigate();
 
@@ -12,31 +12,31 @@ export default function AllPlayers () {
             try {
                 const response = await fetch("https://fsa-puppy-bowl.herokuapp.com/api/2306-GHP-ET-WEB-PT-SF/players");
                 const dataObj = await response.json();
-               
+
                 console.log(dataObj);
                 setPlayerList(dataObj.data.players);
 
-            } catch(e) {
+            } catch (e) {
                 console.log("unable to fetch");
             }
         }
-        fetchAllPlayers();  
+        fetchAllPlayers();
     }, []
     )
 
     return (
-       <div>
-        {playerList && playerList.map(function(puppy) {
-            return (
-                <>
-          <div key={puppy.id}>
-           <h3> Name: {puppy.name} </h3>;
-            <img src={puppy.imageUrl} alt={puppy.name} />  
-            
-            <button onClick={()=>{navigate(`/players/${puppy.id}`)}}>More Info!</button>
-            </div>
-            </>)
-       }) }
+        <div>
+            {playerList && playerList.map(function (puppy) {
+                return (
+                    <>
+                        <div key={puppy.id}>
+                            <h3> Name: {puppy.name} </h3>;
+                            <img src={puppy.imageUrl} alt={puppy.name} />
+
+                            <button onClick={() => { navigate(`/players/${puppy.id}`) }}>More Info!</button>
+                        </div>
+                    </>)
+            })}
         </div>
-     )
+    )
 }
--- src/components/SinglePlayer.jsx
+++ src/components/SinglePlayer.jsx
@@ -2,36 +2,43 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 
-export default function SinglePlayer () {
-    const [] = useState(null)
+export default function SinglePlayer() {
+    const [individualPlayer, setIndividualPlayer] = useState(null)
     const params = useParams()
-    
+
     const playerId = params.id
 
     useEffect(() => {
-       async function fetchSinglePlayer () {
+        async function fetchSinglePlayer() {
             try {
                 const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2306-GHP-ET-WEB-PT-SF/players/${playerId}`);
                 const dataObj = await response.json();
                 const singlePlayer = (dataObj.data.player);
                 console.log(singlePlayer);
 
+                setIndividualPlayer(singlePlayer);
 
 
-            } catch(e){
+            } catch (e) {
                 console.error("did not fetch single player")
             }
 
         }
         fetchSinglePlayer();
     }, []
-    ) 
+    )
     return (
         <>
-        
-        
-        
-        
+            {individualPlayer ? (
+                <div>
+                    <h3>Name: {individualPlayer.name}</h3>
+                    <p>Breed: {individualPlayer.breed}</p>
+                    <p>Status: {individualPlayer.status}</p>
+                    <img src={individualPlayer.imageUrl} alt={individualPlayer.name} />
+
+
+
+                </div>) : <p>loading...</p>}
         </>
     )
 }
